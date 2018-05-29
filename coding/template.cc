e41235d75af275ef05af01e759be8ce020792605
Joel <joeluchoa@gmail.com>
2018-05-29 14:42:56 -0300

    clean

--- coding/template.cc
+++ coding/template.cc
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-#define rep(i,n) for (int (i)=0; (i)<(n); ++(i))
-#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for (int i=0; i<(n); ++i)
+#define all(x) x.begin(),x.end()
 #define dbg(x) cerr << #x << " = " << x << endl
-#define sz(x) (int)((x).size())
+#define sz(x) (int)(x.size())
 #define ms(m,v) memset((m), (v), sizeof (m))
 #define mp make_pair
 #define pb push_back
